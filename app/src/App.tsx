0e56a667712e02af77b4944da96b538c749170e1
JulianKimmig <julian.kimmig@gmx.net>
2024-10-09 08:16:22 +0200

    app id

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,7 +3,7 @@ import FuncnodesReactFlow from "@linkdlab/funcnodes_react_flow";
 
 import "@linkdlab/funcnodes_react_flow/../../css/style.css";
 
-const App = () => {
+const App = ({ id }: { id?: string }) => {
   return (
     <div
       className="App"
@@ -12,7 +12,7 @@ const App = () => {
         width: "100vw",
       }}
     >
-      <FuncnodesReactFlow></FuncnodesReactFlow>
+      <FuncnodesReactFlow id={id}></FuncnodesReactFlow>
     </div>
   );
 };
