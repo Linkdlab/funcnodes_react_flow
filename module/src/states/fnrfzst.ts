c6475e80b18872b6959427eef8dd5a01416c388f
JulianKimmig <julian.kimmig@gmx.net>
2025-02-20 08:22:02 +0100

    feat: add logging for node and edge operations in React Flow Zustand

--- module/src/states/fnrfzst.ts
+++ module/src/states/fnrfzst.ts
@@ -187,6 +187,9 @@ const FuncNodesReactFlowZustand = (
       }
 
       const node = store.getState();
+
+      iterf.logger.info("Add node", node.id, node.name);
+
       const new_ndoes = [
         ...rfstate.nodes,
         assert_reactflow_node(node, store, iterf),
@@ -257,6 +260,7 @@ const FuncNodesReactFlowZustand = (
   };
 
   const _delete_node = (action: NodeActionDelete) => {
+    iterf.logger.info("Deleting node", action.id);
     if (action.from_remote) {
       rfstore.getState().onNodesChange([
         {
@@ -355,6 +359,7 @@ const FuncNodesReactFlowZustand = (
         if (action.from_remote) {
           const edges = rfstate.edges;
           const del_edge_id = generate_edge_id(action);
+          iterf.logger.info("Deleting edge", del_edge_id);
           const new_edges = edges.filter((edge) => edge.id !== del_edge_id);
           rfstore.setState({ edges: new_edges });
           iterf.worker?.get_remote_node_state(action.src_nid);
