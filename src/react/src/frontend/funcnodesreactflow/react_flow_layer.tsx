0a642d67d9a200d24cf37b4a78b1edd7c718fecf
JulianKimmig <julian.kimmig@gmx.net>
2025-05-16 11:45:26 +0200

    feat: implement copy and paste functionality in KeyHandler for nodes and edges

--- src/react/src/frontend/funcnodesreactflow/react_flow_layer.tsx
+++ src/react/src/frontend/funcnodesreactflow/react_flow_layer.tsx
@@ -13,6 +13,7 @@ import {
   useReactFlow,
   Node,
   NodeTypes,
+  OnNodesChange,
 } from "@xyflow/react";
 
 import { FuncNodesContext } from "../funcnodesreactflow";
@@ -26,6 +27,7 @@ import {
 } from "../../states/fnrfzst.t";
 
 import { latest } from "../../types/versioned/versions.t";
+import { update_node } from "../../states/node/update_node";
 
 // import { useForceGraph } from "../../utils/autolayout";
 
@@ -52,11 +54,98 @@ const ReactFlowManager = () => {
   return <></>;
 };
 
-const KeyHandler = () => {
+const KeyHandler = ({ onNodesChange }: { onNodesChange: OnNodesChange }) => {
   const fnrf_zst = useContext(FuncNodesContext);
   const delPressed = useKeyPress("Delete");
+  const copyPressed = useKeyPress(["Meta+c", "Control+c", "Strg+c"]);
+  const pastePressed = useKeyPress(["Meta+v", "Control+v", "Strg+v"]);
   const edges = useEdges();
   const nodes = useNodes();
+
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+
+  const handlePaste = async (e: React.ClipboardEvent<HTMLTextAreaElement>) => {
+    const pastedText = e.clipboardData?.getData("text");
+    if (!pastedText) return;
+    console.log("paste", pastedText);
+    if (!pastedText) return;
+    const copydata: {
+      nodes: latest.SerializedNodeType[];
+      edges: latest.SerializedEdge[];
+    } = JSON.parse(pastedText);
+    console.log("copydata", copydata);
+    if (!copydata) return;
+    if (!fnrf_zst.worker) return;
+    if (!copydata.nodes) return;
+
+    const mean_position = [0, 0];
+    for (const node of copydata.nodes) {
+      mean_position[0] += node.properties["frontend:pos"][0];
+      mean_position[1] += node.properties["frontend:pos"][1];
+    }
+    mean_position[0] /= copydata.nodes.length;
+    mean_position[1] /= copydata.nodes.length;
+
+    const rel_node_infos: {
+      id: string;
+      src_id: string;
+      position: [number, number];
+      new_id?: string;
+    }[] = [];
+    for (const node of copydata.nodes) {
+      const rel_node_info: {
+        id: string;
+        src_id: string;
+        position: [number, number];
+      } = {
+        id: node.node_id,
+        src_id: node.id,
+        position: [
+          node.properties["frontend:pos"][0] - mean_position[0],
+          node.properties["frontend:pos"][1] - mean_position[1],
+        ],
+      };
+      rel_node_infos.push(rel_node_info);
+    }
+
+    for (const node of rel_node_infos) {
+      const new_node = await fnrf_zst.worker.add_node(node.id);
+      console.log("new_node", new_node);
+      if (!new_node) continue;
+      const newnodestore = fnrf_zst.nodespace.get_node(new_node.id, false);
+      if (!newnodestore) continue;
+      node.new_id = new_node.id;
+
+      onNodesChange([
+        {
+          id: new_node.id,
+          type: "position",
+          position: {
+            x: node.position[0] + new_node.properties["frontend:pos"][0],
+            y: node.position[1] + new_node.properties["frontend:pos"][1],
+          },
+        },
+      ]);
+    }
+
+    for (const edge of copydata.edges) {
+      const src_node = rel_node_infos.find(
+        (node) => node.src_id === edge.src_nid
+      );
+      const trg_node = rel_node_infos.find(
+        (node) => node.src_id === edge.trg_nid
+      );
+      if (!src_node || !trg_node) continue;
+      if (!src_node.new_id || !trg_node.new_id) continue;
+      fnrf_zst.worker.add_edge({
+        src_nid: src_node.new_id,
+        src_ioid: edge.src_ioid,
+        trg_nid: trg_node.new_id,
+        trg_ioid: edge.trg_ioid,
+      });
+    }
+  };
+
   if (delPressed) {
     for (const edge of edges) {
       if (edge.selected) {
@@ -77,9 +166,55 @@ const KeyHandler = () => {
         fnrf_zst.worker.remove_node(node.id);
       }
     }
+  } else if (copyPressed) {
+    const copydata: {
+      nodes: latest.SerializedNodeType[];
+      edges: latest.SerializedEdge[];
+    } = { nodes: [], edges: [] };
+    for (const node of nodes) {
+      if (node.selected) {
+        const fnnode = fnrf_zst.nodespace.get_node(node.id, false);
+        if (!fnnode) continue;
+        copydata.nodes.push(fnnode.serialize());
+      }
+    }
+    for (const edge of edges) {
+      if (!edge.source || !edge.target) continue;
+      if (!edge.sourceHandle || !edge.targetHandle) continue;
+      // check if edge.source and edge.target are selected
+      if (
+        nodes.find((node) => node.id === edge.source)?.selected &&
+        nodes.find((node) => node.id === edge.target)?.selected
+      ) {
+        copydata.edges.push({
+          src_nid: edge.source,
+          src_ioid: edge.sourceHandle,
+          trg_nid: edge.target,
+          trg_ioid: edge.targetHandle,
+        });
+      }
+    }
+
+    // inject copydata into clipboard
+    navigator.clipboard.writeText(JSON.stringify(copydata));
+  } else if (pastePressed) {
+    console.log("paste pressed");
+    if (!inputRef.current) return;
+    if (inputRef.current instanceof HTMLTextAreaElement) {
+      inputRef.current.focus();
+      inputRef.current.select();
+    }
   }
 
-  return <></>;
+  return (
+    <>
+      <textarea
+        ref={inputRef}
+        onPaste={handlePaste}
+        style={{ position: "absolute", opacity: 0, pointerEvents: "none" }}
+      />
+    </>
+  );
 };
 
 type ContextMenuProps = {
@@ -190,7 +325,7 @@ const ReactFlowLayer = (props: ReactFlowLayerProps) => {
         panOnDrag={!props.static}
       >
         <ReactFlowManager />
-        <KeyHandler />
+        <KeyHandler onNodesChange={onNodesChange} />
         <Background
           color="#888" // Color of the grid lines
           gap={16} // Distance between grid lines
