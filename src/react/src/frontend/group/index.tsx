89445a89dad4977d6e27efebab2869e175dc5a4a
JulianKimmig <julian.kimmig@gmx.net>
2025-07-16 20:52:34 +0200

    refactor(group): ♻️ reorganize component structure for clarity

--- src/react/src/frontend/group/index.tsx
+++ src/react/src/frontend/group/index.tsx
@@ -1,25 +1,29 @@
 import * as React from "react";
 import { useContext } from "react";
-import { removeGroup } from "../utils/grouping";
-import { FuncNodesContext } from "./funcnodesreactflow";
-import { CloseIcon } from "./assets/fontawsome";
+import { removeGroup } from "../../utils/grouping";
+import { FuncNodesContext } from "../funcnodesreactflow";
+import { CloseIcon } from "../assets/fontawsome";
 
 // The default Node rendering component for groups
 export const DefaultGroup = ({ data }: { data: any }) => {
-    const fnrf_zst = useContext(FuncNodesContext);
-    const groupId = data?.group?.id || data?.id;
-    const handleRemove = (e: React.MouseEvent) => {
-        e.stopPropagation();
-        if (groupId) {
-            removeGroup([groupId], fnrf_zst);
-        }
-    };
-    return (
-        <div className="fn-group">
-            <button className="fn-group-remove" title="Remove group" onClick={handleRemove}>
-                <CloseIcon />
-            </button>
-            Group
-        </div>
-    );
+  const fnrf_zst = useContext(FuncNodesContext);
+  const groupId = data?.group?.id || data?.id;
+  const handleRemove = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    if (groupId) {
+      removeGroup([groupId], fnrf_zst);
+    }
+  };
+  return (
+    <div className="fn-group">
+      <button
+        className="fn-group-remove"
+        title="Remove group"
+        onClick={handleRemove}
+      >
+        <CloseIcon />
+      </button>
+      Group
+    </div>
+  );
 };
