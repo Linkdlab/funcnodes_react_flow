156392a60239a221f1d658d748ac693a1b227ce3
Julian Kimmig <julian.kimmig@gmx.net>
2025-07-17 17:25:34 +0200

    refactor(funcnodescontext): 🛠️ simplify context initialization and error handling
    - Updated `FuncNodesContext` to initialize with `null` instead of a default factory function, enhancing flexibility.
    - Improved error message in `useFuncNodesContext` to clarify usage within a provider.

--- src/react/src/app/providers/funcnodescontext.tsx
+++ src/react/src/app/providers/funcnodescontext.tsx
@@ -1,19 +1,14 @@
 import { createContext, useContext } from "react";
-import {
-  FuncNodesReactFlowZustand,
-  FuncNodesReactFlowZustandInterface,
-} from "../../barrel_imports";
-import { DEFAULT_FN_PROPS_FACTORY } from "../app-properties";
+import { FuncNodesReactFlowZustandInterface } from "../../barrel_imports";
+
 export const FuncNodesContext =
-  createContext<FuncNodesReactFlowZustandInterface>(
-    FuncNodesReactFlowZustand(DEFAULT_FN_PROPS_FACTORY())
-  );
+  createContext<FuncNodesReactFlowZustandInterface | null>(null);
 
 export const useFuncNodesContext = () => {
   const context = useContext(FuncNodesContext);
   if (!context) {
     throw new Error(
-      "useFuncNodesContext must be used within a FuncNodesContext"
+      "useFuncNodesContext must be used within a FuncNodesContext.Provider"
     );
   }
   return context;
